4ca7d5772da734c2ba236a38d752af8ca75d7a21
mverri <verrier.mathilde92@gmail.com>
2014-09-29 21:59:29 +0200

    Liste de radiobuton + bibliotheque
    Creation des radios button en fonction de la difficulté;
    la chanson solution aparait sur un radio buton aléatoire;
    ajout d'une fenetre de dialogue pour choisir un repertoire qui
    correspondra a la bibliotheque de musique avec laquelle on veut jouer,
    le bouton valider et les radio buton sont maintenant créés dans la
    fonction de lancement de la fenetre.

--- BlindTestGroupe44/BlindTestGroupe44/MainWindow.xaml.cs
+++ BlindTestGroupe44/BlindTestGroupe44/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,22 +24,17 @@ namespace BlindTestGroupe44
     {
         private MusicPlayer player = new MusicPlayer();
 
-        //private String repertoireMusique = "C:\\Users\\Chantal\\Desktop\\Michael Jackson";
-        private String repertoireMusique = "I:\\Music\\C2C\\2012 - Tetra";
-        private int nbChoix = 0;
-        private int incrPoints = 0;
+        private String repertoireMusique = ""; // racine de la bibliotheque
+        private int nbChoix = 0; // nb de choix de reponse en fonction de la difficulté
+        private int incrPoints = 0; // incrementation des points en fonction de la difficulté
         private int scorePoints = 0;
         private String nomChanson = "";
-        /*
-         * 
-         * Problemes
-         * Génération de bouton dynamique
-         * Utiliser un panel plutot qu'une grille serait probablement plus adapté
-         * 
-         */ 
+        private int choixCorrect; // index de la bonne reponse
+      
         public MainWindow()
-        {
+        {           
             InitializeComponent();
+            commencerBoutton.IsEnabled = false;
         }
 
         /// <summary>
@@ -49,24 +45,25 @@ namespace BlindTestGroupe44
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+        /// 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             if (this.facileButton.IsChecked == true)
             {
                 nbChoix = 3;
-                incrPoints = 10;
+                incrPoints = 10;               
             }
 
             if (this.moyenButton.IsChecked == true)
             {
                 nbChoix = 4;
-                incrPoints = 12;
+                incrPoints = 12;               
             }
             if (this.difficileButton.IsChecked == true)
             {
                 nbChoix = 6;
-                incrPoints = 15;
-            }
+                incrPoints = 15;              
+            }          
 
             /* Si l'un des boutons est coché on peux passer à l'étape supérieur et lancer le test
              */
@@ -79,49 +76,110 @@ namespace BlindTestGroupe44
             
 
         }
+
+        // Creation des n radiobutton représentant les réponses possible
+        // le parametre n correspond au nbchoix ( en fonction de la difficulté)
+        private void creationRadioButtons(int n)
+        {
+            int y = 126;
+            for (int i = 0; i < n; i++)
+            {
+                System.Windows.Controls.RadioButton r = new System.Windows.Controls.RadioButton();                
+                r.Margin = new Thickness(256, 126 +i*40, 0, 0);               
+                grid2.Children.Add(r);                        
+                y = y + 40;               
+            }
+            System.Windows.Controls.Button valider = new System.Windows.Controls.Button();
+            valider.Click += Button_Click;
+            valider.Content = "Valider";
+            valider.Width = 164;
+            valider.Height = 61;
+            valider.Margin = new Thickness(256, y, 0, 0);
+            valider.Visibility = Visibility.Visible;
+            valider.IsEnabled = true;
+            grid2.Children.Add(valider);
+
+        }
+
+       
+        // Initialise la fenetre du jeu, lancement de la musique, affichage des réponses possibles
         private void initialiseTest(object sender, RoutedEventArgs e)
         {
-
-            grid1.Visibility = Visibility.Hidden;
+            creationRadioButtons(nbChoix);
+            grid1.Visibility = Visibility.Hidden;         
             grid2.Visibility = Visibility.Visible;
-
             player.open(repertoireMusique);
             player.play();
             trouveAleatoire();
-
         }
 
+        //initialise le contenu des radioButtons avec un nom de chanson (aléatoire) de la bibliothèque choisie
+        //1 des radioButtons contiendra la solution a trouver
         private void trouveAleatoire()
         {
             Random rnd = new Random();
-            int place = rnd.Next(0, 4); // la on laissera comme ça quand y'aura une liste
-            place = 1; // en attendant on met le 1; (place 2);
-            r2.Content = player.getChanson() ;
-            try
+            int place = rnd.Next(1, nbChoix);         
+            var songsList = player.listeChanson(repertoireMusique);
+            for (int i = 1; i <= nbChoix; i++ )
             {
-                // Tant qu'on a pas une liste on vérifie pas les doublons
-                var songsList = player.listeChanson(repertoireMusique);
-                r1.Content = songsList.ElementAt(rnd.Next(0, songsList.Count())).Name;
-                r3.Content = songsList.ElementAt(rnd.Next(0, songsList.Count())).Name;
-                r4.Content = songsList.ElementAt(rnd.Next(0, songsList.Count())).Name;
-
-            }
-            catch { }
+                if (i == place)
+                {
+                   getRi(i).Content = player.getChanson();
+                   choixCorrect = i;
+                }
+                else
+                {
+                    getRi(i).Content = songsList.ElementAt(rnd.Next(0, songsList.Count())).Name;
+                }               
+            }        
         }
 
+        // recupere le RadioButton a l'index i dans la grille
+        private System.Windows.Controls.RadioButton getRi(int i)
+        {
+           var radios = grid2.Children.OfType<System.Windows.Controls.RadioButton>();       
+            switch (i)
+            {
+                case 1:
+                    return radios.ElementAt(0);
+                case 2:
+                    return radios.ElementAt(1);
+                case 3:
+                    return radios.ElementAt(2);
+                case 4:
+                    return radios.ElementAt(3);
+                case 5:
+                    return radios.ElementAt(4);
+                case 6:
+                    return radios.ElementAt(5);
+                default:
+                    return radios.ElementAt(1);
+            }
+        }
+
+        //correspond au bouton valider, si on a la bonne reponse, le score augmente
+        // puis on passe a la chanson suivante
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             player.stop();
             // Tant qu'on a pas de liste on vérifie R2
-            if (r2.IsChecked == true)
+            if (getRi(this.choixCorrect).IsChecked == true)
             {
                 scorePoints += incrPoints;
                 scoreLabel.Content = "Score : " + scorePoints;
             }
-
-            chansonPrecedente.Content = player.getChanson();
+            chansonPrecedente.Content = "reponse precedente : " + player.getChanson();
             initialiseTest(sender, e);
-
         }
+
+        private void choisirBibli(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog() ;
+            fbd.ShowDialog();
+            repertoireMusique = fbd.SelectedPath;
+            commencerBoutton.IsEnabled = true;
+        }
+
+
     }
 }
