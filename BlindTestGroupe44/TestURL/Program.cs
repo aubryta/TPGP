191d8617b9d8831be1131bff9d6f3e7c85c52c26
mverri <verrier.mathilde92@gmail.com>
2014-10-17 18:09:36 +0200

    TestURL( fonctions listStyles, listChansons, getChanson)
    Creation des fonctions qui permette de recupérer les different style
    disponible a partir de l'URL racine, et a partir d'un style, récupérer
    la liste des chansons existante.
    Une derniere fonction renvoie a partir d'un titre et d'un style l'URL
    qui servira au client pour lire en interne la chanson

---
+++ BlindTestGroupe44/TestURL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TestURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GestionURL gestion = new GestionURL();
+            List<String> listchanson = gestion.listeChansons("Electro");
+            foreach (String chanson in listchanson)          
+                Console.WriteLine(chanson);
+            Console.ReadLine();
+
+        }
+    }
+}
